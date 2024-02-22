81fbf14d38289ad356a790639d16aed0034d9980
Jay <jay.m.rathod.01@gmail.com>
2024-02-23 04:51:11 +0530

    Dashboard & Navigation UI completed
    Signed-off-by: Jay <jay.m.rathod.01@gmail.com>

--- app/signIn.js
+++ app/signIn.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function SignIn() {
     return (
         <View className="h-full flex-1  justify-center items-center">
-            <Text className="text-3xl ">SignIn</Text>
+            <Text className="text-3xl">SignIn</Text>
         </View>
     );
 }
