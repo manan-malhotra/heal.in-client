d8f5666cf807cd3d5e8d88e4e5e171e0669eb7e6
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-03-20 16:12:56 +0530

    Fetching blogs via backend

--- app/(app)/blog.js
+++ app/(app)/blog.js
@@ -1,129 +1,210 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
-import blogsData from '../../data/blog_content.json';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {LinearGradient} from 'expo-linear-gradient';
-
+import React, { useEffect, useState } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+} from "react-native";
+import blogsData from "../../data/blog_content.json";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { LinearGradient } from "expo-linear-gradient";
+import axios from "axios";
 
 const BlogPage = () => {
-  const [expandedIndex, setExpandedIndex] = useState(null);
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        const loadBlogs = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8082/admin/getAllBlogs"
+                );
+                const blog = response.data;
+                const blogDatas = [];
+                blog.map((blog) => {
+                    const index = blog.blog_id;
+                    const title = blog.title;
+                    const author =
+                        blog.user_id.first_name + " " + blog.user_id.last_name;
+                    const originalDateString = blog.post_date;
+                    const originalDate = new Date(originalDateString);
 
-  const handleViewMore = (index) => {
-    setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
+                    const monthNames = [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December",
+                    ];
 
-  const handleReportClick = () => {
-    // Handle report click action here
-    console.log('Report clicked');
-  };
+                    const month = monthNames[originalDate.getMonth()];
+                    const day = originalDate.getDate();
+                    const year = originalDate.getFullYear();
+                    const formattedDate = `${month} ${day}, ${year}`;
+                    const date = formattedDate;
+                    const content = blog.description;
+                    blogDatas.push({
+                        index,
+                        title,
+                        author,
+                        date,
+                        content,
+                    });
+                });
+                setBlogs(blogDatas);
+            } catch (error) {
+                // Handle error
+                setBlogs(blogsData);
+            }
+        };
+        loadBlogs();
+    }, []);
 
-  const gradientColors = ['rgba(255,255,255,0.2)', 'rgba(110,113,254,0.6)', 'rgba(4,0,207,0.4)'];
+    const [expandedIndex, setExpandedIndex] = useState(null);
 
-  return (
-    <ScrollView>
-      {/* Body */}
-      <LinearGradient colors={gradientColors}  style={styles.gradient}>
-      <View style={styles.body}>
-        <Text style={styles.heading}>Our Blogs</Text>
-        
-        {/* Blog List */}
-        <View style={styles.blogList}>
-          {/* Map through the blogsData array and render each blog */}
-          {blogsData.map((blog, index) => (
-            <View style={styles.blog} key={index}>
-              <Text style={styles.title}>{blog.title}</Text>
-              <Text style={styles.author}>- {blog.author}</Text>
-              <Text>
-                {/* Display only the first few lines of content */}
-                {expandedIndex === index ? blog.content : blog.content.length > 100 ? blog.content.substring(0, 100) + "......" : blog.content}
-                {/* Render "View More" button only if content is longer than 100 characters */}
-                {blog.content.length > 100 && (
-                  <Text style={styles.viewMoreButton} onPress={() => handleViewMore(index)}>
-                    {expandedIndex === index ? 'View Less' : ' View More'}
-                  </Text>
-                )}
-              </Text>
-              <Text style={styles.date}>{blog.date}</Text>
-              <TouchableOpacity style={styles.reportButton} onPress={handleReportClick}>
-                <Icon name="flag" size={15} color="#ccc" /> 
-              </TouchableOpacity>
-            </View>
-          ))}
-        </View>
-      </View>
-      </LinearGradient>
-    </ScrollView>
-  );
+    const handleViewMore = (index) => {
+        setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
+    };
+
+    const handleReportClick = () => {
+        // Handle report click action here
+        console.log("Report clicked");
+    };
+
+    const gradientColors = [
+        "rgba(255,255,255,0.2)",
+        "rgba(110,113,254,0.6)",
+        "rgba(4,0,207,0.4)",
+    ];
+
+    return (
+        <LinearGradient colors={gradientColors} style={styles.gradient}>
+            <ScrollView>
+                {/* Body */}
+                <View style={styles.body}>
+                    <Text style={styles.heading}>Our Blogs</Text>
+
+                    {/* Blog List */}
+                    <View style={styles.blogList}>
+                        {/* Map through the blogsData array and render each blog */}
+                        {blogs.map((blog, index) => (
+                            <View style={styles.blog} key={index}>
+                                <Text style={styles.title}>{blog.title}</Text>
+                                <Text style={styles.author}>
+                                    - {blog.author}
+                                </Text>
+                                <Text>
+                                    {/* Display only the first few lines of content */}
+                                    {expandedIndex === index
+                                        ? blog.content
+                                        : blog.content.length > 100
+                                        ? blog.content.substring(0, 100) +
+                                          "......"
+                                        : blog.content}
+                                    {/* Render "View More" button only if content is longer than 100 characters */}
+                                    {blog.content.length > 100 && (
+                                        <Text
+                                            style={styles.viewMoreButton}
+                                            onPress={() =>
+                                                handleViewMore(index)
+                                            }
+                                        >
+                                            {expandedIndex === index
+                                                ? "View Less"
+                                                : " View More"}
+                                        </Text>
+                                    )}
+                                </Text>
+                                <Text style={styles.date}>{blog.date}</Text>
+                                <TouchableOpacity
+                                    style={styles.reportButton}
+                                    onPress={handleReportClick}
+                                >
+                                    <Icon name="flag" size={15} color="#ccc" />
+                                </TouchableOpacity>
+                            </View>
+                        ))}
+                    </View>
+                </View>
+            </ScrollView>
+        </LinearGradient>
+    );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    padding: 20,
-  }, 
-  heading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  blogList: {
-    marginTop: 10,
-  },
-  blog: {
-    marginBottom: 20,
-    backgroundColor: 'rgba(0,0,255,0.07)',
-    borderRadius: 8,
-    padding: 20,
-    marginBottom: 20,
-    
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  author: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#005B55',
-    marginBottom: 5,
-  },
-  content: {
-    fontSize: 16,
-    fontWeight: 'medium',
-  },
-  date: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#005B55',
-    marginTop: 10,
-  },
-  viewMoreButton: {
-    color: '#005B55',
-    position: 'absolute',
-    fontWeight: 'bold',
-    bottom: 10,
-    right: 10,
-  },
-  viewMore: {
-    color: 'green',
-  },
-  reportButton: {
-    position: 'absolute',
-    bottom: 8,
-    right: 8,
-    backgroundColor: 'rgba(255,0,0,0.7)',
-    paddingVertical: 5,
-    paddingHorizontal: 7,
-    borderRadius: 8,
-    elevation: 5,
-  },
-  report: {
-    color: '#ff3131',
-    fontSize: 5,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
+    body: {
+        flex: 1,
+        padding: 20,
+    },
+    heading: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    blogList: {
+        marginTop: 10,
+    },
+    blog: {
+        marginBottom: 20,
+        backgroundColor: "rgba(0,0,255,0.07)",
+        borderRadius: 8,
+        padding: 20,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    author: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "#005B55",
+        marginBottom: 5,
+    },
+    content: {
+        fontSize: 16,
+        fontWeight: "medium",
+    },
+    date: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "#005B55",
+        marginTop: 10,
+    },
+    viewMoreButton: {
+        color: "#005B55",
+        position: "absolute",
+        fontWeight: "bold",
+        bottom: 10,
+        right: 10,
+    },
+    viewMore: {
+        color: "green",
+    },
+    reportButton: {
+        position: "absolute",
+        bottom: 8,
+        right: 8,
+        backgroundColor: "rgba(255,0,0,0.7)",
+        paddingVertical: 5,
+        paddingHorizontal: 7,
+        borderRadius: 8,
+        elevation: 5,
+    },
+    report: {
+        color: "#ff3131",
+        fontSize: 5,
+        fontWeight: "bold",
+        marginTop: 10,
+    },
 });
 
 export default BlogPage;
--- context/authcontext.js
+++ context/authcontext.js
@@ -5,109 +5,113 @@ import axios from "axios";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(undefined);
 
-  useEffect(() => {
-    const loadToken = async () => {
-      const token = await AsyncStorage.getItem("token");
-      console.log(typeof token);
-      setIsAuthenticated(!!token); // Set isAuthenticated to true if token exists, false otherwise
-      if (token) {
-        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    useEffect(() => {
+        const loadToken = async () => {
+            const token = await AsyncStorage.getItem("token");
+            console.log(typeof token);
+            setIsAuthenticated(!!token); // Set isAuthenticated to true if token exists, false otherwise
+            if (token) {
+                axios.defaults.headers.common[
+                    "Authorization"
+                ] = `Bearer ${token}`;
 
+                try {
+                    const profileResponse = await axios.get(
+                        "http://localhost:8082/api/user/getProfile"
+                    );
+                    if (profileResponse.status === 200) {
+                        console.log(profileResponse.data);
+                        const data = profileResponse.data;
+                        const strAge = String(data.age);
+                        const strcontact = String(data.contact);
+                        await AsyncStorage.setItem("firstName", data.firstName);
+                        await AsyncStorage.setItem("age", strAge);
+                        await AsyncStorage.setItem("lastName", data.lastName);
+                        await AsyncStorage.setItem("email", data.email);
+                        await AsyncStorage.setItem("gender", data.gender);
+                        await AsyncStorage.setItem("role", data.role);
+                        await AsyncStorage.setItem("contact", strcontact);
+                    }
+                } catch {
+                    await AsyncStorage.removeItem("token");
+                    await AsyncStorage.removeItem("firstName");
+                    await AsyncStorage.removeItem("age");
+                    await AsyncStorage.removeItem("lastName");
+                    await AsyncStorage.removeItem("email");
+                    await AsyncStorage.removeItem("gender");
+                    await AsyncStorage.removeItem("role");
+                    await AsyncStorage.removeItem("contact");
+                    axios.defaults.headers.common["Authorization"] = "";
+                    setIsAuthenticated(false);
+                }
+            }
+        };
+        loadToken();
+    }, [isAuthenticated]);
+
+    const login = async (email, password) => {
         try {
-          const profileResponse = await axios.get(
-            "https://4a21-106-51-167-211.ngrok-free.app/api/user/getProfile"
-          );
-          if (profileResponse.status === 200) {
-            console.log(profileResponse.data);
-            const data = profileResponse.data;
-            const strAge = String(data.age);
-            const strcontact = String(data.contact);
-            await AsyncStorage.setItem("firstName", data.firstName);
-            await AsyncStorage.setItem("age", strAge);
-            await AsyncStorage.setItem("lastName", data.lastName);
-            await AsyncStorage.setItem("email", data.email);
-            await AsyncStorage.setItem("gender", data.gender);
-            await AsyncStorage.setItem("role", data.role);
-            await AsyncStorage.setItem("contact", strcontact);
-          }
-        } catch {
-          await AsyncStorage.removeItem("token");
-          await AsyncStorage.removeItem("firstName");
-          await AsyncStorage.removeItem("age");
-          await AsyncStorage.removeItem("lastName");
-          await AsyncStorage.removeItem("email");
-          await AsyncStorage.removeItem("gender");
-          await AsyncStorage.removeItem("role");
-          await AsyncStorage.removeItem("contact");
-          axios.defaults.headers.common["Authorization"] = "";
-          setIsAuthenticated(false);
+            const response = await axios.post(
+                "http://localhost:8082/api/user/login",
+                {
+                    email: email,
+                    password: password,
+                }
+            );
+
+            if (response.status === 200) {
+                const token = response.data;
+                await AsyncStorage.setItem("token", token);
+                axios.defaults.headers.common[
+                    "Authorization"
+                ] = `Bearer ${token}`;
+                setIsAuthenticated(true);
+            }
+        } catch (error) {
+            console.log("Error logging in: ", error);
+            setIsAuthenticated(false);
         }
-      }
     };
-    loadToken();
-  }, [isAuthenticated]);
 
-  const login = async (email, password) => {
-    try {
-      const response = await axios.post(
-        "https://4a21-106-51-167-211.ngrok-free.app/api/user/login",
-        {
-          email: email,
-          password: password,
+    const register = async (username, password, email) => {
+        try {
+        } catch (error) {}
+    };
+    const logout = async () => {
+        try {
+            await AsyncStorage.removeItem("token");
+            await AsyncStorage.removeItem("token");
+            await AsyncStorage.removeItem("firstName");
+            await AsyncStorage.removeItem("age");
+            await AsyncStorage.removeItem("lastName");
+            await AsyncStorage.removeItem("email");
+            await AsyncStorage.removeItem("gender");
+            await AsyncStorage.removeItem("role");
+            await AsyncStorage.removeItem("contact");
+            axios.defaults.headers.common["Authorization"] = "";
+            setIsAuthenticated(false);
+        } catch (error) {
+            console.error("Error logging out: ", error);
         }
-      );
-
-      if (response.status === 200) {
-        const token = response.data;
-        await AsyncStorage.setItem("token", token);
-        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-        setIsAuthenticated(true);
-      }
-    } catch (error) {
-      console.log("Error logging in: ", error);
-      setIsAuthenticated(false);
-    }
-  };
-
-  const register = async (username, password, email) => {
-    try {
-    } catch (error) {}
-  };
-  const logout = async () => {
-    try {
-      await AsyncStorage.removeItem("token");
-      await AsyncStorage.removeItem("token");
-      await AsyncStorage.removeItem("firstName");
-      await AsyncStorage.removeItem("age");
-      await AsyncStorage.removeItem("lastName");
-      await AsyncStorage.removeItem("email");
-      await AsyncStorage.removeItem("gender");
-      await AsyncStorage.removeItem("role");
-      await AsyncStorage.removeItem("contact");
-      axios.defaults.headers.common["Authorization"] = "";
-      setIsAuthenticated(false);
-    } catch (error) {
-      console.error("Error logging out: ", error);
-    }
-  };
-  return (
-    <AuthContext.Provider
-      value={{ user, isAuthenticated, login, register, logout }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    };
+    return (
+        <AuthContext.Provider
+            value={{ user, isAuthenticated, login, register, logout }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuth = () => {
-  const value = useContext(AuthContext);
-  const { isAuthenticated } = value;
-  console.log(isAuthenticated + " test");
-  if (!value) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return value;
+    const value = useContext(AuthContext);
+    const { isAuthenticated } = value;
+    console.log(isAuthenticated + " test");
+    if (!value) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return value;
 };
