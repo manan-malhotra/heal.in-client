9e20d01857218398940f7cd2cd5d0ad3a6f5bbdd
Yash Talele <yashrtalele@gmail.com>
2024-03-21 18:36:36 +0530

    Changed MessageItems views from className to stylesheets

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,25 +1,72 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export default function MessageItem({message, currentUserId, currentUsername}) {
     if(currentUserId == message?.userId) {
         return (
-            <View className="flex-row justify-end mb-3 mr-3">
-                <View style={{width: wp(80)}}>
-                    <View className="flex self-end p-3 rounded-2xl bg-white border border-neutral-200">
-                        <Text style={{fontSize: hp(1.9)}}>{message?.text}</Text>
-                    </View>
-                </View>
+            <View style={styles_me.container}>
+            <View style={styles_me.outerView}>
+              <View style={styles_me.innerView}>
+                <Text style={styles_me.text}>{message?.text}</Text>
+              </View>
             </View>
+          </View>
         )
     } else {
         return (
-            <View stle={{width: wp(80)}} className="ml-3 mb-3">
-                <View className="flex self-start p-3 px-4 rounded-2xl border bg-indigo-100 border-indigo-200">
-                    <Text style={{fontSize: hp(1.9)}}>{message?.text}</Text>
+            <View style={styles_other.container}>
+                <View style={styles_other.outerView}>
+                    <View style={styles_other.innerView}>
+                    <Text style={styles_other.text}>{message?.text}</Text>
+                    </View>
                 </View>
             </View>
         )
     }
 }
+
+const styles_other = StyleSheet.create({
+    container: {
+        width: wp(80),
+        marginLeft: 12, 
+        marginBottom: 12,
+    },
+    outerView: {
+    },
+    innerView: {
+        alignSelf: 'flex-start',
+        paddingVertical: 12, 
+        paddingHorizontal: 16,
+        borderRadius: 16,
+        backgroundColor: 'rgb(224, 231, 255)',
+        borderWidth: 1,
+        borderColor: 'rgb(199, 210, 254)',
+    },
+    text: {
+        fontSize: hp(1.9),
+    },
+});
+
+const styles_me = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      marginBottom: 12,
+      marginRight: 12,
+    },
+    outerView: {
+        width: wp(80)
+    },
+    innerView: {
+      alignSelf: 'flex-end',
+      padding: 12,
+      borderRadius: 16,
+      backgroundColor: 'white',
+      borderWidth: 1,
+      borderColor: 'rgb(229, 229, 229)',
+    },
+    text: {
+      fontSize: hp(1.9),
+    },
+});
