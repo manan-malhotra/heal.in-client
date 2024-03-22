685c631f5671d4186b07978b6b17c80a6ed4880c
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-03-22 09:57:53 +0530

    Removing firebase analysis

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,17 +8,17 @@ import { collection, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGFPzKkIrNXLaP-Cw4vP7L6oKjFHQzTsY",
-  authDomain: "healin-45885.firebaseapp.com",
-  projectId: "healin-45885",
-  storageBucket: "healin-45885.appspot.com",
-  messagingSenderId: "157661679962",
-  appId: "1:157661679962:web:ae3a392c182abb6bffa363",
-  measurementId: "G-2V2FXPF80M",
+    apiKey: "AIzaSyCGFPzKkIrNXLaP-Cw4vP7L6oKjFHQzTsY",
+    authDomain: "healin-45885.firebaseapp.com",
+    projectId: "healin-45885",
+    storageBucket: "healin-45885.appspot.com",
+    messagingSenderId: "157661679962",
+    appId: "1:157661679962:web:ae3a392c182abb6bffa363",
+    measurementId: "G-2V2FXPF80M",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
