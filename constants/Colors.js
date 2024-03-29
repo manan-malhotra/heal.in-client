d775707cba85aa6a1a77dcb248ec03d5153e382e
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-03-29 19:11:53 +0530

    Color palette added

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,13 +1,15 @@
 import { DefaultTheme } from "react-native-paper";
 
 const theme = {
-  DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    text: "#000000",
-    primary: "#6E71FE",
-    secondary: "#1877F2",
-  },
+    DefaultTheme,
+    colors: {
+        text: "#000000",
+        primary: "#456990",
+        secondary: "#114b5f",
+        button: "028090",
+        background: "#f9fcf9",
+        error: "#f45b69",
+    },
 };
 
 export { theme };
