57dee05cf778d6348fe05796177f9893ad7a0d5c
Yash <yashrtalele@gmail.com>
2024-04-02 10:04:19 +0530

    Added a page to the admin side to add tests

---
+++ app/admin/tests/addTests.js
@@ -0,0 +1,52 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { theme } from "../../../constants/Colors";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import AddCards from "../../../components/AddCards";
+import Title from "../../../components/Title";
+
+const AddTests = () => {
+  return (
+    <View style={styles.body}>
+      <View style={{ paddingBottom: 0 }}></View>
+      <Title title="Add Tests" />
+      <View style={{ backgroundColor: "#ffffff", flex: 1 }}>
+        <AddCards
+          name="ADHD"
+          icon="book-open"
+          route="/admin/tests/addTestFields"
+        />
+        <AddCards
+          name="Anxiety"
+          icon="book-open"
+          route="/admin/tests/addTestFields"
+        />
+        <AddCards
+          name="Depression"
+          icon="book-open"
+          route="/admin/tests/addTestFields"
+        />
+        <AddCards
+          name="Postpartum"
+          icon="book-open"
+          route="/admin/tests/addTestFields"
+        />
+        <AddCards
+          name="Youth Mental Health"
+          icon="book-open"
+          route="/admin/tests/addTestFields"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingTop: hp(10),
+  },
+});
+
+export default AddTests;
