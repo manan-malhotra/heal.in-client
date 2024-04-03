236de605666eadb61183b96b45c20609e9123f90
Yash <yashrtalele@gmail.com>
2024-04-04 00:39:03 +0530

    Added Profile Page

--- app/user/home/_layout.js
+++ app/user/home/_layout.js
@@ -3,70 +3,76 @@ import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 
 const _layout = () => {
-    const user = useLocalSearchParams();
-    return (
-        <Stack>
-            <Stack.Screen
-                name="index"
-                initialParams={user}
-                options={{
-                    headerTitle: "heal.in",
-                }}
-            />
-            <Stack.Screen
-                name="selfHelpVideos"
-                options={{
-                    headerTitle: "",
-                    headerShown: false,
-                    headerBackTitleVisible: false,
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: "white",
-                    },
-                    headerTintColor: "black",
-                }}
-            />
-            <Stack.Screen
-                name="blogs/index"
-                initialParams={user}
-                options={{
-                    headerTitle: "Blogs",
-                    headerBackTitleVisible: false,
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: "white",
-                    },
-                    headerTintColor: "black",
-                }}
-            />
-            <Stack.Screen
-                name="forums"
-                initialParams={user}
-                options={{
-                    headerTitle: "Forums",
-                    headerBackTitleVisible: false,
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: "white",
-                    },
-                    headerTintColor: "black",
-                }}
-            />
-            <Stack.Screen
-                name="newForum"
-                initialParams={user}
-                options={{
-                    headerTitle: "Forums",
-                    headerBackTitleVisible: false,
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: "white",
-                    },
-                    headerTintColor: "black",
-                }}
-            />
-        </Stack>
-    );
+  const user = useLocalSearchParams();
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        initialParams={user}
+        options={{
+          headerTitle: "heal.in",
+        }}
+      />
+      <Stack.Screen
+        name="selfHelpVideos"
+        options={{
+          headerTitle: "",
+          headerShown: false,
+          headerBackTitleVisible: false,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerTintColor: "black",
+        }}
+      />
+      <Stack.Screen
+        name="blogs/index"
+        initialParams={user}
+        options={{
+          headerTitle: "Blogs",
+          headerBackTitleVisible: false,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerTintColor: "black",
+        }}
+      />
+      <Stack.Screen
+        name="forums"
+        initialParams={user}
+        options={{
+          headerTitle: "Forums",
+          headerBackTitleVisible: false,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerTintColor: "black",
+        }}
+      />
+      <Stack.Screen
+        name="newForum"
+        initialParams={user}
+        options={{
+          headerTitle: "Forums",
+          headerBackTitleVisible: false,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerTintColor: "black",
+        }}
+      />
+      <Stack.Screen
+        name="relax"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default _layout;
