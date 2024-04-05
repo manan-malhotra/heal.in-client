a326ec5d3100a7b361f178304f1ea5b6fca5ccec
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-05 11:11:20 +0530

    Admin axios minor fix

--- app/admin/_layout.js
+++ app/admin/_layout.js
@@ -3,70 +3,73 @@ import { Tabs, useLocalSearchParams } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 
 export default function _layout() {
-  const user = useLocalSearchParams();
-  return (
-    <>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarShowLabel: false,
-          tabBarHideOnKeyboard: true,
-        }}
-      >
-        <Tabs.Screen
-          name="home"
-          options={{
-            title: "Add Users",
-            tabBarIcon: ({ focused, color, size }) => (
-              <Feather
-                name="user-plus"
-                size={24}
-                color={focused ? "black" : "#adadad"}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="tests"
-          options={{
-            title: "Add Tests",
-            tabBarIcon: ({ focused, color, size }) => (
-              <Feather
-                name="edit"
-                size={24}
-                color={focused ? "black" : "#adadad"}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="resources"
-          options={{
-            title: "Add Resources",
-            tabBarIcon: ({ focused, color, size }) => (
-              <Feather
-                name="airplay"
-                size={24}
-                color={focused ? "black" : "#adadad"}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="adminProfile"
-          initialParams={user}
-          options={{
-            title: "Admin Profile",
-            tabBarIcon: ({ focused, color, size }) => (
-              <Feather
-                name="user"
-                size={24}
-                color={focused ? "black" : "#adadad"}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-    </>
-  );
+    const user = useLocalSearchParams();
+    return (
+        <>
+            <Tabs
+                screenOptions={{
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarHideOnKeyboard: true,
+                }}
+            >
+                <Tabs.Screen
+                    name="home"
+                    initialParams={user}
+                    options={{
+                        title: "Add Users",
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Feather
+                                name="user-plus"
+                                size={24}
+                                color={focused ? "black" : "#adadad"}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="tests"
+                    initialParams={user}
+                    options={{
+                        title: "Add Tests",
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Feather
+                                name="edit"
+                                size={24}
+                                color={focused ? "black" : "#adadad"}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="resources"
+                    initialParams={user}
+                    options={{
+                        title: "Add Resources",
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Feather
+                                name="airplay"
+                                size={24}
+                                color={focused ? "black" : "#adadad"}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="adminProfile"
+                    initialParams={user}
+                    options={{
+                        title: "Admin Profile",
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Feather
+                                name="user"
+                                size={24}
+                                color={focused ? "black" : "#adadad"}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
+    );
 }
