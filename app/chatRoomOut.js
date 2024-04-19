16bb4f026886a69fa98f2d7c68daa26f7bfcf7b7
Yash <yashrtalele@gmail.com>
2024-04-19 11:28:16 +0530

    Removed unwanted functions

--- app/chatRoomOut.js
+++ app/chatRoomOut.js
@@ -48,15 +48,6 @@ const getRoomId = (userId1, userId2) => {
   const roomId = sortedIds.join("-");
   return roomId;
 };
-function createRandomString(length) {
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
 function generateRoomId(length) {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
