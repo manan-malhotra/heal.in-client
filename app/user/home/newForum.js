603b16f4e115a43931a41a42a6a361ea249824db
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-21 18:38:06 +0530

    Sentiment analysis check

--- app/user/home/newForum.js
+++ app/user/home/newForum.js
@@ -1,12 +1,12 @@
 import {
-    Alert,
-    KeyboardAvoidingView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
+  Alert,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { theme } from "../../../constants/Colors";
@@ -14,128 +14,128 @@ import axios from "axios";
 import { router, useLocalSearchParams } from "expo-router";
 import { sentimentScore } from "../../../common/sentiment";
 const NewForum = () => {
-    const user = useLocalSearchParams();
-    const [query, setQuery] = useState("");
-    const handleSubmit = async () => {
-        if (query.trim() === "") {
-            return;
+  const user = useLocalSearchParams();
+  const [query, setQuery] = useState("");
+  const handleSubmit = async () => {
+    if (query.trim() === "") {
+      return;
+    }
+    try {
+      const score = await sentimentScore(query);
+      if (score < -0.2) {
+        Alert.alert(
+          "Warning",
+          "Please refrain from using negative words in your query.",
+          [{ text: "Ok" }]
+        );
+        return;
+      }
+      const response = await axios.post(
+        process.env.API_HOST + "/api/user/addQuestion",
+        {
+          question: query,
+          userId: user.userId,
+          description: "",
         }
-        try {
-            const score = await sentimentScore(query);
-            if (score < -0.2) {
-                Alert.alert(
-                    "Warning",
-                    "Please refrain from using negative words in your query.",
-                    [{ text: "Ok" }]
-                );
-                return;
-            }
-            const response = await axios.post(
-                process.env.API_HOST + "/api/user/addQuestion",
-                {
-                    question: query,
-                    userId: user.userId,
-                    description: "",
-                }
-            );
-            if (response.status === 200) {
-                console.log("success");
-                router.dismissAll();
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <View style={styles.body}>
-            <ScrollView style={styles.question}>
-                <View style={styles.query}>
-                    <TextInput
-                        placeholder="Ask your question..."
-                        placeholderTextColor={theme.colors.primary}
-                        style={styles.queryText}
-                        onKeyPress={(e) => {
-                            if (e.nativeEvent.key === "Enter") {
-                                handleSubmit();
-                            }
-                        }}
-                        onChangeText={(text) => {
-                            setQuery(text);
-                        }}
-                        multiline
-                    />
-                </View>
-            </ScrollView>
-            <KeyboardAvoidingView behavior="padding">
-                <TouchableOpacity
-                    style={styles.submit}
-                    onPress={() => {
-                        handleSubmit();
-                    }}
-                >
-                    <Text style={styles.submitText}>Submit</Text>
-                </TouchableOpacity>
-            </KeyboardAvoidingView>
+      );
+      if (response.status === 200) {
+        console.log("success");
+        router.dismissAll();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <View style={styles.body}>
+      <ScrollView style={styles.question}>
+        <View style={styles.query}>
+          <TextInput
+            placeholder="Ask your question..."
+            placeholderTextColor={theme.colors.primary}
+            style={styles.queryText}
+            onKeyPress={(e) => {
+              if (e.nativeEvent.key === "Enter") {
+                handleSubmit();
+              }
+            }}
+            onChangeText={(text) => {
+              setQuery(text);
+            }}
+            multiline
+          />
         </View>
-    );
+      </ScrollView>
+      <KeyboardAvoidingView behavior="padding">
+        <TouchableOpacity
+          style={styles.submit}
+          onPress={() => {
+            handleSubmit();
+          }}
+        >
+          <Text style={styles.submitText}>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
 };
 
 export default NewForum;
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        backgroundColor: "white",
-        paddingTop: "5%",
-        paddingHorizontal: "5%",
-    },
-    question: {
-        width: "100%",
-        flex: 1,
-        height: "80%",
-        // backgroundColor: theme.colors.background,
-        borderRadius: 10,
-        marginTop: "5%",
-        borderColor: theme.colors.primary,
-        borderWidth: 1,
-    },
-    query: {
-        marginTop: "4%",
-        padding: "7%",
-        height: "80%",
-    },
-    submitText: {
-        fontSize: 20,
-        fontWeight: "bold",
-        color: "black",
-        textAlign: "center",
-        zIndex: 1,
-    },
-    queryText: {
-        fontSize: 16,
-        fontWeight: "500",
-        letterSpacing: 0.5,
-        height: 500,
-    },
-    submit: {
-        width: "45%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        borderRadius: 10,
-        backgroundColor: theme.colors.primary,
-        paddingBottom: "2%",
-        paddingTop: "2%",
-        borderColor: theme.colors.primary,
-        borderWidth: 1,
-        marginTop: "10%",
-        borderRadius: 10,
-        marginBottom: "5%",
-    },
-    submitText: {
-        fontSize: 20,
-        fontWeight: "bold",
-        color: "white",
-        textAlign: "center",
-        zIndex: 1,
-    },
+  body: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: "5%",
+    paddingHorizontal: "5%",
+  },
+  question: {
+    width: "100%",
+    flex: 1,
+    height: "80%",
+    // backgroundColor: theme.colors.background,
+    borderRadius: 10,
+    marginTop: "5%",
+    borderColor: theme.colors.primary,
+    borderWidth: 1,
+  },
+  query: {
+    marginTop: "4%",
+    padding: "7%",
+    height: "80%",
+  },
+  submitText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+    textAlign: "center",
+    zIndex: 1,
+  },
+  queryText: {
+    fontSize: 16,
+    fontWeight: "500",
+    letterSpacing: 0.5,
+    height: 500,
+  },
+  submit: {
+    width: "45%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 10,
+    backgroundColor: theme.colors.primary,
+    paddingBottom: "2%",
+    paddingTop: "2%",
+    borderColor: theme.colors.primary,
+    borderWidth: 1,
+    marginTop: "10%",
+    borderRadius: 10,
+    marginBottom: "5%",
+  },
+  submitText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    zIndex: 1,
+  },
 });
--- common/sentiment.js
+++ common/sentiment.js
@@ -1,15 +1,55 @@
 import axios from "axios";
 const sentimentScore = async (text) => {
-    const json = {
-        content: text,
-    };
-    try {
-        const response = await axios.post(process.env.SENTIMENT_API, json);
-        return response.data.sentiment;
-    } catch (error) {
-        console.log(error);
-        return 0;
+  const json = {
+    content: text,
+  };
+  const bag1 = [
+    "depression",
+    "depressed",
+    "weight",
+    "suicide",
+    "suicidal",
+    "anxiety",
+    "obesity",
+    "self-harm",
+    "despair",
+    "overweight",
+    "self-destructive",
+    "distress",
+    "harm",
+    "suffer",
+    "suffering",
+    "overeating",
+  ];
+  const bag2 = [
+    "reduce",
+    "stop",
+    "decrease",
+    "lose",
+    "minimize",
+    "halt",
+    "diminish",
+    "lower",
+    "cease",
+    "drop",
+  ];
+  const value = text.toLowerCase();
+  for (let i = 0; i < bag1.length; i++) {
+    if (value.includes(bag1[i])) {
+      for (let j = 0; j < bag2.length; j++) {
+        if (value.includes(bag2[j])) {
+          return 0;
+        }
+      }
     }
+  }
+  try {
+    const response = await axios.post(process.env.SENTIMENT_API, json);
+    return response.data.sentiment;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
 };
 
 export { sentimentScore };
